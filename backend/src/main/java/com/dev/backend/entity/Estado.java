f865ee3cf1330f9f03ec41e89cac384124b5043f
santosraquel <raquel.dossantos1965@gmail.com>
2023-04-27 21:44:00 -0300

    criando a classe Estado

---
+++ backend/src/main/java/com/dev/backend/entity/Estado.java
@@ -0,0 +1,30 @@
+package com.dev.backend.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Table(name = "estado") // nome da tabela no banco de dados
+@Data // importação do Lombok, para gerar os getters e setters
+public class Estado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // estratégia de geração da chave primária
+    private Long id;
+
+    private String nome;
+    private String sigla;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCriacao;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataAtualizacao;
+
+}
