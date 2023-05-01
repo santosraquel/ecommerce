bc46e8f3125aaf8a1c72353abc3b18c88b1c670d
santosraquel <raquel.dossantos1965@gmail.com>
2023-05-01 20:57:12 -0300

    testando as rotas de estado no Postman

--- backend/src/main/java/com/dev/backend/controller/EstadoController.java
+++ backend/src/main/java/com/dev/backend/controller/EstadoController.java
@@ -3,15 +3,16 @@ package com.dev.backend.controller;
 import com.dev.backend.entity.Estado;
 import com.dev.backend.service.EstadoService;
 
-import jakarta.websocket.server.PathParam;
-
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,21 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/estado") // definindo uma rota padrão para todos os endpoints
 public class EstadoController {
 
+    @Autowired
     private EstadoService estadoService;
 
     @PostMapping("/")
-    public Estado inserir(Estado estado) {
+    public Estado inserir(@RequestBody Estado estado) {
         return estadoService.inserir(estado);
     }
 
     @PutMapping("/")
-    public Estado alterar(Estado estado) {
+    public Estado alterar(@RequestBody Estado estado) {
         return estadoService.alterar(estado);
     }
 
     // EXCLUIR
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> excluir(@PathParam("id") Long id) {
+    public ResponseEntity<Void> excluir(@PathVariable("id") Long id) {
         estadoService.excluir(id);
         return ResponseEntity.ok().build();
     }
