a495962d49ce6c31dc1885af462a124889cfb06b
santosraquel <raquel.dossantos1965@gmail.com>
2023-05-01 23:46:36 -0300

    arrumando rota de excluir categoria

--- backend/src/main/java/com/dev/backend/controller/CategoriaControler.java
+++ backend/src/main/java/com/dev/backend/controller/CategoriaControler.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,8 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.backend.entity.Categoria;
 import com.dev.backend.service.CategoriaService;
-
-import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/api/categoria")
@@ -38,7 +37,7 @@ public class CategoriaControler {
 
     // EXCLUIR
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> excluir(@PathParam("id") Long id) {
+    public ResponseEntity<Void> excluir(@PathVariable("id") Long id) {
         categoriaService.excluir(id);
         return ResponseEntity.ok().build();
     }
