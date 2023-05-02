a1ebef1a990ec382f2fada34086c95115d349a2b
santosraquel <raquel.dossantos1965@gmail.com>
2023-05-01 23:17:28 -0300

    criando a entidade fornecedor

---
+++ backend/src/main/java/com/dev/backend/entity/Fornecedor.java
@@ -0,0 +1,28 @@
+package com.dev.backend.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Table(name = "fornecedor")
+@Data
+public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCriacao;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataAtualizacao;
+}
