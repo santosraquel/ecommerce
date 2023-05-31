2b0a863e2a983dabd495ea5bff6a4b7dbbf28d92
santosraquel <raquel.dossantos1965@gmail.com>
2023-05-30 21:33:42 -0300

    sobrescrevendo método setPermissaoPessoa

--- backend/src/main/java/com/dev/backend/entity/Pessoa.java
+++ backend/src/main/java/com/dev/backend/entity/Pessoa.java
@@ -1,17 +1,22 @@
 package com.dev.backend.entity;
 
 import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Entity
 @Table(name = "pessoa")
@@ -36,4 +41,15 @@ public class Pessoa {
     @ManyToOne
     @JoinColumn(name = "idCidade")
     private Cidade cidade;
+
+    @OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @Setter(value = AccessLevel.NONE) // desabilirtando o método set que o lombok fornece
+    private List<PermissaoPessoa> permissaoPessoas;
+
+    public void setPermissaoPessoa(List<PermissaoPessoa> pp) {
+        for (PermissaoPessoa p : pp) {
+            p.setPessoa(this); // setando pessoa como o próprio obj da classe
+        }
+        this.permissaoPessoas = pp;
+    }
 }
