1e448deacb1dd82929587535b7739d8aeca7f7d4
santosraquel <raquel.dossantos1965@gmail.com>
2023-06-29 20:32:34 -0300

    registro de cliente

--- backend/src/main/java/com/dev/backend/repository/PermissaoRepository.java
+++ backend/src/main/java/com/dev/backend/repository/PermissaoRepository.java
@@ -3,7 +3,9 @@ package com.dev.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dev.backend.entity.Permissao;
+import java.util.List;
 
 public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
 
+    List<Permissao> findByNome(String nome);
 }
