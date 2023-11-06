45bc7045cfc3debca214c0e2e88e3765e3c65d41
santosraquel <raquel.dossantos1965@gmail.com>
2023-11-06 17:19:21 -0300

    criando método de login

--- backend/src/main/java/com/dev/backend/controller/PessoaGerenciamentoController.java
+++ backend/src/main/java/com/dev/backend/controller/PessoaGerenciamentoController.java
@@ -1,12 +1,18 @@
 package com.dev.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.backend.entity.Pessoa;
+import com.dev.backend.security.JwtUtil;
 import com.dev.backend.service.PessoaGerenciamentoService;
 
 @RestController
@@ -15,6 +21,12 @@ public class PessoaGerenciamentoController {
 
     @Autowired
     private PessoaGerenciamentoService pessoaGerenciamentoService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
 
     // RECUPERAR CÓDIGO
     @PostMapping("/senha-codigo")
@@ -25,6 +37,16 @@ public class PessoaGerenciamentoController {
     @PostMapping("/senha-alterar")
     public String alterarSenha(@RequestBody Pessoa pessoa) {
         return pessoaGerenciamentoService.alterarSenha(pessoa);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Pessoa pessoa) {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(pessoa.getEmail(), pessoa.getSenha()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        Pessoa autenticado = (Pessoa) authentication.getPrincipal();
+        String token = jwtUtil.gerarTokenUsername(autenticado);
+        return ResponseEntity.ok(token);
     }
 
 }
